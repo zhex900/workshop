b9932dbc3aa2ba6847c00941aab1bdf120e23d2f
Jake He <jake.he@nextgen.group>
2026-02-04 10:40:19 +1100

    fix typecheck

--- packages/workshop-app/app/routes/sitemap[.]xml.ts
+++ packages/workshop-app/app/routes/sitemap[.]xml.ts
@@ -1,11 +1,12 @@
 import { generateSitemap } from '@nasa-gcn/remix-seo'
-import type { ServerRouteManifest as RemixServerRouteManifest } from '@remix-run/server-runtime/dist/routes'
 import { type ServerBuild, type LoaderFunctionArgs } from 'react-router'
 import { getDomainUrl } from '#app/utils/misc.tsx'
 
 export async function loader({ request, context }: LoaderFunctionArgs) {
 	const serverBuild = (await context.serverBuild) as ServerBuild
-	const routes = serverBuild.routes as unknown as RemixServerRouteManifest
+	const routes = serverBuild.routes as unknown as Parameters<
+		typeof generateSitemap
+	>[1]
 	return generateSitemap(request, routes, {
 		siteUrl: getDomainUrl(request),
 		headers: {
